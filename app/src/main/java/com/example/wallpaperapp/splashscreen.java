95a0919dba5374c002b9678c7d20718e5cb28cb8
androidtut <krishkharal190@gmail.com>
2022-10-09 23:41:26 -0700

    wallpaper app part 1 splashscreen complete

---
+++ app/src/main/java/com/example/wallpaperapp/splashscreen.java
@@ -0,0 +1,29 @@
+package com.example.wallpaperapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                try{
+                    sleep(5*1000);
+                    startActivity(new Intent(splashscreen.this,MainActivity.class));
+                    finish();
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+                super.run();
+            }
+        };thread.start();
+    }
+}
